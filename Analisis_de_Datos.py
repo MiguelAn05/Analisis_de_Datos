4ec31817af9823c736c48f926976b9f542c239b0
MiguelAn05 <miguelingil89@gmail.com>
2023-12-19 21:39:08 -0500

    se agrega parte 2 y 3

--- Analisis_de_Datos.py
+++ Analisis_de_Datos.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import numpy as np
 import pandas as pd
 
-
+#Parte 1
 dataset = load_dataset("mstz/heart_failure")
 data = dataset["train"]
 
@@ -18,6 +18,9 @@ average_age = np.mean(ages_array)
 
 print(f'El promedio de edad de las personas participantes en el estudio es {average_age}')
 
+
+
+#parte 2
 df = pd.DataFrame(data)
 
 # Separa el DataFrame en dos, uno para las personas que perecieron y otro para las que no
@@ -31,3 +34,17 @@ average_age_alive = df_alive['age'].mean()
 print(f'El promedio de edad de las personas que perecieron en el estudio es {average_age_dead}')
 print(f'El promedio de edad de las personas que sobrevivieron en el estudio es {average_age_alive}')
 
+
+
+
+#parte 3
+# Verificamos los tipos de datos de cada columna
+print(df.dtypes)
+
+df['is_dead'] = df['is_dead'].astype(bool)
+
+hombres_fumadores = df[(df['is_male'] == 1) & (df['is_smoker'] == 1)].shape[0]
+mujeres_fumadoras = df[(df['is_male'] == 0) & (df['is_smoker'] == 1)].shape[0]
+
+print(f'Hombres fumadores: {hombres_fumadores}')
+print(f'Mujeres fumadoras: {mujeres_fumadoras}')
